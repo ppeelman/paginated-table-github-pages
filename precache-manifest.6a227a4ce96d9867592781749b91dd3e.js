596bef1f16e6b441a5d4e2852f606bf35688d061
Philippe Peelman <ppeelman@altaiconsulting.com>
2019-06-06 17:01:50 +0200

    First build of the project

---
+++ precache-manifest.6a227a4ce96d9867592781749b91dd3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e72ca7ad8353453e362c70509a734392",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a7869a93ed6cd8f08fa",
+    "url": "/static/css/main.705139df.chunk.css"
+  },
+  {
+    "revision": "17d04f7b9c2c9abe440b",
+    "url": "/static/js/2.195722e5.chunk.js"
+  },
+  {
+    "revision": "1a7869a93ed6cd8f08fa",
+    "url": "/static/js/main.43421ca8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "be0d1f2b3f8e525f57af5978fd5bf2cb",
+    "url": "/static/media/hero.be0d1f2b.jpg"
+  }
+]);
