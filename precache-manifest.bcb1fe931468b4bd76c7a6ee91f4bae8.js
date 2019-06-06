7f4da1f72ff455882b2e335357f61a91f2172ad9
Philippe Peelman <ppeelman@altaiconsulting.com>
2019-06-06 18:13:25 +0200

    Added lazy loading and redesigned back button

---
+++ precache-manifest.bcb1fe931468b4bd76c7a6ee91f4bae8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e5a76d7fae7c4badc9f71676f0f548",
+    "url": "/paginated-table-github-pages/index.html"
+  },
+  {
+    "revision": "179475fd294c651f37c3",
+    "url": "/paginated-table-github-pages/static/css/3.fac9dc3d.chunk.css"
+  },
+  {
+    "revision": "377719032a70d0411c5c",
+    "url": "/paginated-table-github-pages/static/css/main.5a387e37.chunk.css"
+  },
+  {
+    "revision": "f24c595c8f8527e0e282",
+    "url": "/paginated-table-github-pages/static/js/2.9e492784.chunk.js"
+  },
+  {
+    "revision": "179475fd294c651f37c3",
+    "url": "/paginated-table-github-pages/static/js/3.6c1c9809.chunk.js"
+  },
+  {
+    "revision": "377719032a70d0411c5c",
+    "url": "/paginated-table-github-pages/static/js/main.af168a37.chunk.js"
+  },
+  {
+    "revision": "c9a8d95a2508bb858c6c",
+    "url": "/paginated-table-github-pages/static/js/runtime~main.bce8f845.js"
+  },
+  {
+    "revision": "be0d1f2b3f8e525f57af5978fd5bf2cb",
+    "url": "/paginated-table-github-pages/static/media/hero.be0d1f2b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/paginated-table-github-pages/precache-manifest.f86a41b957ed333da876d72ebb55f5f4.js"
+  "/paginated-table-github-pages/precache-manifest.bcb1fe931468b4bd76c7a6ee91f4bae8.js"
 );
 
 self.addEventListener('message', (event) => {
